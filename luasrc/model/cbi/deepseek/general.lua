d28107857519182a1d0ca90534789b05073bd3ff
ywt114 <58467878+ywt114@users.noreply.github.com>
2025-01-27 18:09:06 +0800

    Update general.lua

--- luasrc/model/cbi/deepseek/general.lua
+++ luasrc/model/cbi/deepseek/general.lua
@@ -1,13 +1,19 @@
 local m = Map("deepseek", 
     translate("DeepSeek Configuration"),
-    translate("Configure DeepSeek service parameters"))
+    translate("Configure parameters for DeepSeek integration"))
 
-local s = m:section(TypedSection, "general", translate("General Settings"))
+local s = m:section(TypedSection, "general", translate("Core Settings"))
 s.anonymous = true
+s.addremove = false
 
-s:option(Flag, "enabled", translate("Enable Service"))
-s:option(Value, "api_key", translate("API Key"))
-s:option(Value, "interval", translate("Check Interval"))
-    .datatype = "uinteger"
+s:option(Flag, "enabled", translate("Enable Service")).default = 0
+
+api_key = s:option(Value, "api_key", translate("API Key"))
+api_key.password = true
+api_key.rmempty = false
+
+interval = s:option(Value, "interval", translate("Update Interval (seconds)"))
+interval.datatype = "range(60,3600)"
+interval.default = 300
 
 return m
